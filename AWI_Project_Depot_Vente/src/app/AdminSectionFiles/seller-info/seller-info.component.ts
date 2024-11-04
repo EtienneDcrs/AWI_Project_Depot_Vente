c7aee2270fb58e1b8a670710a24654713f650589
TLTwizz <etienne.ducros987@gmail.comm>
2024-11-04 19:07:27 +0100

    création de la page d'information pour chaque vendeur

--- AWI_Project_Depot_Vente/src/app/AdminSectionFiles/seller-info/seller-info.component.ts
+++ AWI_Project_Depot_Vente/src/app/AdminSectionFiles/seller-info/seller-info.component.ts
@@ -1,27 +1,35 @@
 import { Component } from '@angular/core';
 import { SellerService } from '../../services/seller.service';
 import { Seller } from '../../../models/Seller';
+import { AdminNavigationComponent } from '../admin-navigation/admin-navigation.component';
+import { Game } from '../../../models/Game';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
     selector: 'app-seller-info',
     standalone: true,
-    imports: [],
+    imports: [AdminNavigationComponent],
     templateUrl: './seller-info.component.html',
     styleUrl: './seller-info.component.css'
 })
 
 export class SellerInfoComponent {
     seller: Seller | undefined;
-    constructor(private sellerService: SellerService) { }
+    sellerStock: Game[] = [];
+    constructor(private sellerService: SellerService, private route: ActivatedRoute ) { 
+        
+    }
 
     //recupère l'id du vendeur dans l'url
     ngOnInit(): void {
-        const id = window.location.href.split('/').pop();
+        const id = this.route.snapshot.paramMap.get('id');
         if (id) {
             this.loadSeller(id);
+            
         } else {
             console.error('ID du vendeur non trouvé dans l\'URL');
         }
+
     }
 
     //charge les informations du vendeur
@@ -30,6 +38,7 @@ export class SellerInfoComponent {
             (data) => {
                 console.log(data);
                 this.seller = data;
+                this.sellerStock = this.seller.getStock();
             },
             (error) => {
                 console.error('Erreur lors de la récupération des informations du vendeur:', error);
