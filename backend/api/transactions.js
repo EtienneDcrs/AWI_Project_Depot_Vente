62f4c228efe2cad7a225f1f0aa999238c6d63a2b
EtienneDcrs <etienne.ducros987@gmail.com>
2025-02-09 17:47:00 +0100

    debug transaction impossible avec conflit sessions

--- backend/api/transactions.js
+++ backend/api/transactions.js
@@ -6,67 +6,71 @@ const router = express.Router();
 
 // Get all transactions
 router.get("/", async (req, res) => {
-    try {
-        const transactions = await Transaction.find();
-        return res.json(transactions);
-    } catch (error) {
-        res.status(500).json({ message: "Error fetching transactions" });
-    }
+  try {
+    const transactions = await Transaction.find();
+    return res.json(transactions);
+  } catch (error) {
+    res.status(500).json({ message: "Error fetching transactions" });
+  }
 });
 
 // Get a single transaction
 router.get("/:id", async (req, res) => {
-    try {
-        const transaction = await Transaction.findOne({ id: req.params.id });
-        if (transaction) {
-            return res.json(transaction);
-        } else {
-            return res.status(404).json({ message: "Transaction not found" });
-        }
-    } catch (error) {
-        res.status(500).json({ message: "Error fetching transaction" });
+  try {
+    const transaction = await Transaction.findOne({ id: req.params.id });
+    if (transaction) {
+      return res.json(transaction);
+    } else {
+      return res.status(404).json({ message: "Transaction not found" });
     }
+  } catch (error) {
+    res.status(500).json({ message: "Error fetching transaction" });
+  }
 });
 
 // Add a new transaction
 router.post("/", async (req, res) => {
-    const transaction = new Transaction({
-        gameId: req.body.gameId,
-        gameName: req.body.gameName,
-        buyerId: req.body.buyerId,
-        buyerName: req.body.buyerName,
-        sellerId: req.body.sellerId,
-        sellerName: req.body.sellerName,
-        date: req.body.date,
-        price: req.body.price,
-    });
-    console.log("transaction", transaction);
-    try {
-        console.log("here");
-        const newTransaction = await transaction.save();
-        // Remove the game from the seller's stock
-        console.log("here");
-        
-        const seller = await Seller.findOne({ id: req.body.sellerId });
-        console.log("seller", seller);
-        if (seller) {
+  const transaction = new Transaction({
+    gameId: req.body.gameId,
+    gameName: req.body.gameName,
+    buyerId: req.body.buyerId,
+    buyerName: req.body.buyerName,
+    sellerId: req.body.sellerId,
+    sellerName: req.body.sellerName,
+    date: req.body.date,
+    price: req.body.price,
+    depositFee: req.body.depositFee,
+    commission: req.body.commission,
+    sessionId: req.body.sessionId,
+  });
+  console.log("transaction", transaction);
+  try {
+    console.log("here");
+    const newTransaction = await transaction.save();
+    // Remove the game from the seller's stock
+    console.log("here");
 
-            seller.stocks = seller.stocks.filter(
-                (stock) => stock !== req.body.gameId
-            );
-            console.log("seller", seller);
-            seller.sales.push(req.body.gameId);
-            console.log("seller", seller);
-            seller.turnover += req.body.price;
-            console.log("seller", seller);
-            await seller.save();
-            return res.status(201).json(newTransaction);
-        } else {
-            return res.status(404).json({ message: "Seller not found" });
-        }
-    } catch (error) {
-        res.status(400).json({ message: "Error creating transaction" });
+    const seller = await Seller.findOne({ id: req.body.sellerId });
+    console.log("seller", seller);
+    if (seller) {
+      seller.stocks = seller.stocks.filter(
+        (stock) => stock !== req.body.gameId
+      );
+      console.log("seller", seller);
+      seller.sales.push(req.body.gameId);
+      console.log("seller", seller);
+      seller.turnover += req.body.price;
+      console.log("seller", seller);
+      await seller.save();
+      return res.status(201).json(newTransaction);
+    } else {
+      console.log("Seller not found");
+      return res.status(404).json({ message: "Seller not found" });
     }
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({ message: "Error creating transaction" });
+  }
 });
 
 export default router;
