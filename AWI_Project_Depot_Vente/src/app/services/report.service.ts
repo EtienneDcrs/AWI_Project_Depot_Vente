3c6af6e6f71d812869f39c54ca66bd6deb57b15f
EtienneDcrs <etienne.ducros987@gmail.com>
2025-02-10 17:10:38 +0100

    changement url localhost de report service

--- AWI_Project_Depot_Vente/src/app/services/report.service.ts
+++ AWI_Project_Depot_Vente/src/app/services/report.service.ts
@@ -2,23 +2,23 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Report } from '../../models/Report';
+import { environment } from '../../environments/environment';
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root',
 })
 export class ReportService {
-    private apiUrl = 'http://localhost:4000/api/report';
+  private apiUrl = environment.backendHostUrl + '/report';
 
-    constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
-    // Fetch the report from the API and return it as an Observable
-    getReport(): Observable<Report> {
-        return this.http.get<Report>(this.apiUrl);
-    }
+  // Fetch the report from the API and return it as an Observable
+  getReport(): Observable<Report> {
+    return this.http.get<Report>(this.apiUrl);
+  }
 
-    // Fetch the session report from the API and return it as an Observable
-    getSessionReport(): Observable<Report> {
-        return this.http.get<Report>(this.apiUrl + '/session');
-    }
-
+  // Fetch the session report from the API and return it as an Observable
+  getSessionReport(): Observable<Report> {
+    return this.http.get<Report>(this.apiUrl + '/session');
+  }
 }
