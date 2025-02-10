5c53097502c0177e0921036d88920316839eb5be
TLTwizz <etienne.ducros987@gmail.comm>
2025-02-10 10:13:14 +0100

    api/report fonctinnel

--- backend/api/report.js
+++ backend/api/report.js
@@ -1,20 +1,47 @@
 import express from "express";
 import Report from "../models/Report.js";
-import Transaction from "../models/Transaction.js";
+import Game from "../models/Game.js";
+import Seller from "../models/Seller.js";
+import Buyer from "../models/Buyer.js";
 
 const router = express.Router();
 
 router.get("/", async (req, res) => {
     try {
-        const allTransactions = await Transaction.find();
-        
-    
-    
-    }
-    catch (error) {
-        res.status(500).json({ message: "Error fetching transactions" });
+        const gamesSold = await Game.find({ status: "vendu" });
+        const gamesSoldNumber = gamesSold.length;
+        const totalSales = gamesSold.reduce((sum, game) => sum + game.price, 0);
+        const gamesInStock = await Game.find({ status: "stock" });
+        const gamesInStockNumber = gamesInStock.length;
+        const potentialSales =
+            totalSales +
+            gamesInStock.reduce((sum, game) => sum + game.price, 0);
+        const commissionsEarnings = gamesSold.reduce(
+            (sum, game) => sum + game.commission,
+            0
+        );
+        const depositEarnings = gamesSold.reduce(
+            (sum, game) => sum + game.depositFee,
+            0
+        );
+        const totalBuyers = (await Seller.find()).length;
+        const totalSellers = (await Buyer.find()).length;
+
+        const report = new Report({
+            totalSales,
+            gamesSoldNumber,
+            gamesInStockNumber,
+            potentialSales,
+            commissionsEarnings,
+            depositEarnings,
+            totalBuyers,
+            totalSellers,
+        });
+        return res.json(report);
+    } catch (error) {
+        console.error("Error fetching report:", error);
+        res.status(500).json({ message: "Error fetching report" });
     }
 });
-
 
 export default router;
--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,19 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-import cors from 'cors';
-import games from './api/games.js';
-import transactions from './api/transactions.js';
-import sellers from './api/sellers.js';
-import buyers from './api/buyers.js';
-import stocks from './api/stocks.js';
-import report from './api/report.js';
-import sessions from './api/sessions.js';
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import cors from "cors";
+import games from "./api/games.js";
+import transactions from "./api/transactions.js";
+import sellers from "./api/sellers.js";
+import buyers from "./api/buyers.js";
+import stocks from "./api/stocks.js";
+import report from "./api/report.js";
+import sessions from "./api/sessions.js";
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 4000;
-
 
 // // Middleware to check roles (Define this before using it)
 // function checkRole(requiredRole) {
@@ -47,21 +46,22 @@ app.use(express.json());
 app.use(cors());
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGODB_URI)
+mongoose
+    .connect(process.env.MONGODB_URI)
     .then(() => {
-        console.log('Connected to MongoDB');
+        console.log("Connected to MongoDB");
     })
     .catch((err) => {
-        console.error('Error connecting to MongoDB:', err);
+        console.error("Error connecting to MongoDB:", err);
     });
 
-app.use('/api/games', games);
-app.use('/api/transactions', transactions);
-app.use('/api/sellers', sellers);
-app.use('/api/buyers', buyers);
-app.use('/api/stocks', stocks);
-app.use('api/report', report);
-app.use('/api/sessions', sessions);
+app.use("/api/games", games);
+app.use("/api/transactions", transactions);
+app.use("/api/sellers", sellers);
+app.use("/api/buyers", buyers);
+app.use("/api/stocks", stocks);
+app.use("/api/report", report);
+app.use("/api/sessions", sessions);
 
 app.listen(port, () => {
     console.log(`Server running on http://localhost:${port}`);
